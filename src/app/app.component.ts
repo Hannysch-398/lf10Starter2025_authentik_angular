f28d3ec5b4174f0d1b8c1ddba716d351bcc8b93f
Alena <alvi22334@gmail.com>
2026-01-14 10:54:48 +0100

    Angular Update2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {EmployeeListComponent} from "./employee-list/employee-list.component";
 import { HeaderComponent } from './header/header.component';
-
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, HeaderComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, EmployeeListComponent, RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'lf10StarterNew';
