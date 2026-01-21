e062725de6237ce932d7f13098a21172518f19d7
Alena <alvi22334@gmail.com>
2026-01-21 08:28:21 +0100

    add employee, angular update

--- src/app/employee-add/employee-add.component.ts
+++ src/app/employee-add/employee-add.component.ts
@@ -1,22 +1,105 @@
-import {Component, signal} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {Employee} from "../Employee";
-import { FormField, FieldState, FieldTree, form, maxLength, min, minLength, required, schema, submit } from '@angular/forms/signals';
+import {Field, form, FormField, pattern, required} from "@angular/forms/signals";
+import {Form, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {AuthService} from "../auth.service";
+import {EmployeeListComponent} from "../employee-list/employee-list.component";
+
+
 
 @Component({
   selector: 'app-employee-add',
   standalone: true,
-  imports: [],
+  imports: [
+    Field,
+    FormField,
+    EmployeeListComponent
+  ],
   templateUrl: './employee-add.component.html',
   styleUrl: './employee-add.component.css'
 })
 export class EmployeeAddComponent {
 
-  addModel = signal({
-    firstName: '',
+  empModel = signal({
     lastName: '',
+    firstName:'',
+    street: '',
+    postcode: '',
+    city:'',
+    phone:'',
 
-  });
+  })
+  empForm = form(this.empModel, (schemaPath) => {
+      required(schemaPath.firstName, {
+        message: 'Vornamen eingeben'
+      });
+      required(schemaPath.lastName, {
+        message: 'Nachname eingeben'
+      });
+    required(schemaPath.street, {
+      message: 'Strasse eingeben'
+    });
+    required(schemaPath.postcode, {
+      message: 'Postleitzahl eingeben'
+    });
+    pattern(schemaPath.postcode, /[0-9]/, {
+      message: 'Nur Zahlen erlaubt'
+    });
+    required(schemaPath.city, {
+      message: 'Stadt eingeben'
+    });
+    required(schemaPath.phone, {
+      message: 'Telefonnummer eingeben'
+    });
+    pattern(schemaPath.phone, /[0-9]/, {
+      message: 'Nur Zahlen erlaubt'
+    });
+    }
 
-  addForm = form(this.addModel);
+  );
+
+  //employeeList = inject(EmployeeListComponent);
+
+  private http = inject(HttpClient);
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  submitForm() {
+    const emp: Employee = new Employee(
+      undefined, // id wird vom Backend gesetzt
+      this.empForm.lastName(),
+      this.empForm.firstName(),
+      this.empForm.street(),
+      this.empForm.postcode(),
+      this.empForm.city(),
+      this.empForm.phone()
+    );
+
+   //this.employeeList.addEmployee(emp);
+
+  }
+
+  resetForm() {
+    // alle Felder auf den ursprünglichen empModel-Wert zurücksetzen
+    this.empModel.set({
+      firstName: '',
+      lastName: '',
+      street: '',
+      postcode: '',
+      city: '',
+      phone: ''
+    });
+
+    // zusätzlich die Form-Fehler zurücksetzen
+    this.empForm().reset();
+  }
+
+  ariaInvalidState(field: any) {
+    return field.invalid() && field.touched();
+  }
+
 
 }
+
