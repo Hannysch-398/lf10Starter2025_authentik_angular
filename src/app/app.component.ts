2c0a7ec0275052974351ecd6cb7adbaa6ee5017d
hannah.schneider <hannah.schneider@hmmh.de>
2026-01-21 10:08:44 +0100

    angular version 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    imports: [CommonModule, RouterOutlet, HeaderComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lf10StarterNew';
