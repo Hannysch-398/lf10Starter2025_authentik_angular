5ebacee65f69a760a67ae7402b097eda1cb57f31
hannah.schneider <hannah.schneider@hmmh.de>
2026-02-04 09:38:29 +0100

    clean up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,5 @@
-import {Routes} from '@angular/router';
-// import { HomeComponent } from './home/home.component';
-// import { EmployeeListComponent } from './employee-list/employee-list.component';
-// import { CallbackComponent } from './callback/callback.component';
-// import { authGuard } from './auth.guard';
 import {AccountComponent} from "./account/account.component";
 import {Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
 import {EmployeeListComponent} from './employee-list/employee-list.component';
 import {CallbackComponent} from './callback/callback.component';
 import {authGuard} from './auth.guard';
@@ -22,12 +16,9 @@ export const routes: Routes = [
     children: [
       {path: '', redirectTo: 'home', pathMatch: 'full'},
       {path: 'employees', component: EmployeeListComponent, title: 'Mitarbeiter'},
-      // {path: 'home', component: HomeComponent, title: 'Home'},
       {path: "account", component: AccountComponent, title: "Account"},
       {path: 'employees', component: EmployeeListComponent, title: 'Mitarbeiter'},
       {path: 'home', component: OverviewComponent, title: 'Home'},
       // {path: "qualifications", component:QualificationComponent, title: "Qualifikationen"},
-      // {path: "qualifications", component:QualificationComponent, title: "Qualifikationen"},
-      // {path:"account", component: accountComponent, title: "Account"}
     ],
   }]
